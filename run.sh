0878758445d6d076e8641afc794ab47e0bb03ae1
kbrother <00076289@kaist.ac.kr>
2023-08-07 06:56:10 +0000

    minor changes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python main.py -tp ../input/23-Irregular-Tensor/cms.npy -fp parafac2/cms_factor.mat -op results/cms.txt -de 0 -e 500 -lr 0.01 &
-python main.py -tp ../input/23-Irregular-Tensor/ml_sample.npy -fp parafac2/ml_sample_factor.mat -op results/ml_sample.txt -de 1 -e 500 -lr 0.01 &
+python main.py -tp ../input/23-Irregular-Tensor/cms.npy -fp parafac2/cms_factor.mat -op results/cms-gd.txt -de 0 -e 500 -lr 0.01 &
+python main.py -tp ../input/23-Irregular-Tensor/ml-1m.npy -fp parafac2/ml-1m-factor.mat -op results/ml-1m-gd.txt -de 1 -e 500 -lr 0.01 &
 wait
