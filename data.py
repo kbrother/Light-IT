d1299e0865bada438f4b8e002549c997bed473c2
kbrother <00076289@kaist.ac.kr>
2023-08-07 05:15:45 +0000

    fast data laod + only U

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import itertools
 
 class irregular_tensor:
     
@@ -12,24 +13,29 @@ class irregular_tensor:
         self.i_max = np.max(self.i)
                         
         # upload data to gpu
-        self.row, self.col, self.height, self.vals = [], [], [], []
+        self.rows, self.cols, self.heights, self.vals = [], [], [], []
         self.num_nnz = 0
         for _k in range(self.k):
-            _row, _col = self.src_tensor[_k].nonzero()
-            _vals = self.src_tensor[_k][_row, _col]
+            _rows, _cols = self.src_tensor[_k].nonzero()
+            _vals = self.src_tensor[_k][_rows, _cols]
             if _vals.size > 1:
                 _vals = np.asarray(_vals).squeeze()                          
-            self.row = self.row + _row.tolist()
-            self.col = self.col + _col.tolist()
+            self.rows.append(_rows.tolist())            
+            self.cols.append(_cols.tolist())
             if _vals.size == 1:                
-                self.vals = self.vals + [_vals[0]]
+                self.vals.append([_vals[0]])
             else:
-                self.vals = self.vals + _vals.tolist()
-            self.height = self.height + [_k] * _row.size
+                self.vals.append(_vals.tolist())
+            self.heights.append([_k] * _rows.size)  
             self.num_nnz += self.src_tensor[_k].count_nonzero()
             
-        self.rows = torch.tensor(self.row, device=device, dtype=torch.long)
-        self.cols = torch.tensor(self.col, device=device, dtype=torch.long)    
+        self.rows = list(itertools.chain.from_iterable(self.rows))
+        self.cols = list(itertools.chain.from_iterable(self.cols))
+        self.heights = list(itertools.chain.from_iterable(self.heights))
+        self.vals = list(itertools.chain.from_iterable(self.vals))
+        
+        self.rows = torch.tensor(self.rows, device=device, dtype=torch.long)
+        self.cols = torch.tensor(self.cols, device=device, dtype=torch.long)    
         self.vals = torch.tensor(self.vals, device=device)
-        self.heights = torch.tensor(self.height, device=device, dtype=torch.long)                
+        self.heights = torch.tensor(self.heights, device=device, dtype=torch.long)                
         self.sq_sum = torch.sum(torch.square(self.vals)).item()
