c47ca46a4d717cd6c42cd0c3a9a70e1e87676527
kbrother <00076289@kaist.ac.kr>
2023-10-07 09:53:27 +0000

    data end

--- data.py
+++ data.py
@@ -1,47 +1,49 @@
 import numpy as np
 import torch
 import itertools
+import pickle
 
 class irregular_tensor:
     
-    def __init__(self, input_path, device, is_dense):
-        # Initialize variable
-        self.src_tensor = np.load(input_path, allow_pickle=True)
-        self.k = len(self.src_tensor)
-        self.j = self.src_tensor[0].shape[1]
-        self.i = np.array([self.src_tensor[_i].shape[0] for _i in range(self.k)])
-        self.i_max = np.max(self.i)
-        
+    def __init__(self, input_path, is_dense):        
         self.sq_sum = 0
         if is_dense:
-            for i in range(self.k):
+            # Initialize variable
+            self.src_tensor = np.load(input_path, allow_pickle=True)
+            self.num_tensor = len(self.src_tensor)
+            self.mode = len(self.src_tensor[0].shape) + 1
+            self.middle_dim = self.src_tensor[0].shape[1:] 
+            self.first_dim = np.array([self.src_tensor[_i].shape[0] for _i in range(self.num_tensor)])
+            self.max_first = np.max(self.first_dim)
+
+            for i in range(self.num_tensor):
                 self.sq_sum += np.sum(np.square(self.src_tensor[i]))
 
         # upload data to gpu        
         if not is_dense:
-            self.rows_list, self.cols_list, self.heights_list, self.vals_list = [], [], [], []
-            self.num_nnz = 0
-            for _k in range(self.k):
-                _rows, _cols = self.src_tensor[_k].nonzero()
-                _vals = self.src_tensor[_k][_rows, _cols]
-                if _vals.size > 1:
-                    _vals = np.asarray(_vals).squeeze()                          
-                self.rows_list.append(_rows.tolist())            
-                self.cols_list.append(_cols.tolist())
-                if _vals.size == 1:                
-                    self.vals_list.append([float(_vals)])
-                else:
-                    self.vals_list.append(_vals.tolist())
-                self.heights_list.append([_k] * _rows.size)  
-                self.num_nnz += self.src_tensor[_k].count_nonzero()
-
-            self.rows = list(itertools.chain.from_iterable(self.rows_list))
-            self.cols = list(itertools.chain.from_iterable(self.cols_list))
-            self.heights = list(itertools.chain.from_iterable(self.heights_list))
-            self.vals = list(itertools.chain.from_iterable(self.vals_list))
-
-            self.rows = torch.tensor(self.rows, device=device, dtype=torch.long)
-            self.cols = torch.tensor(self.cols, device=device, dtype=torch.long)    
-            self.vals = torch.tensor(self.vals, device=device)
-            self.heights = torch.tensor(self.heights, device=device, dtype=torch.long)                
-            self.sq_sum = torch.sum(torch.square(self.vals)).item()
+            with open(input_path, 'rb') as f:
+                raw_dict = pickle.load(f)            
+            self.indices = raw_dict['idx']
+            self.num_nnz = len(self.indices)
+            self.values = np.array(raw_dict['val'])
+            self.mode = len(self.indices)
+            
+            # sort indices            
+            for m in range(self.mode):
+                self.indices[m] = np.array(self.indices[m])
+            
+            idx2newidx = np.argsort(self.indices[-1])
+            for m in range(self.mode):
+                self.indices[m] = self.indices[m][idx2newidx]
+            
+            # save tensor stat        
+            self.max_first = max(self.indices[0]) + 1
+            self.num_tensor = max(self.indices[-1]) + 1
+            self.middle_dim = []
+            for m in range(1, self.mode-1):
+                self.middle_dim.append(max(self.indices[m]) + 1)                                                                                                      
+            self.sq_sum = np.sum(self.values**2)            
+            self.tidx2start = [0]
+            for i in range(self.num_nnz):
+                if self.indices[-1][self.tidx2start[-1]] != self.indices[-1][i]:
+                    self.tidx2start.append(i)
