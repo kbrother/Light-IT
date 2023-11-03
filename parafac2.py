725793c766052001e59d8ae4ea93e635eb02b590
kbrother <00076289@kaist.ac.kr>
2023-11-03 08:05:33 +0000

    L2 loss

--- parafac2.py
+++ parafac2.py
@@ -170,27 +170,36 @@ class parafac2:
         curr_tensor = torch.reshape(curr_tensor, (end_idx - start_idx, -1))   # bs' x i_2 * ... * i_(m-1) 
         return curr_tensor
         
-                                        
+        
+    '''
+        Return a tensor of size 'batch size' x j_1*j_2*...*j_(d-2)        
+    '''
+    def set_curr_tensor_new(self, batch_size, start_i):        
+        curr_tensor = self.tensor.src_tensor_torch[start_i:start_i + batch_size].to(self.device)  # bs' x i_2 x ... x i_(m-1)  
+        curr_tensor = torch.reshape(curr_tensor, (batch_size, -1))   # bs' x i_2 * ... * i_(m-1) 
+        return curr_tensor
+    
+    
     '''
         input_U: num_tensor x i_max x rank
     '''
     def L2_loss_dense(self, args, is_train, mode):
         _loss = 0            
-        for i in tqdm(range(0, self.tensor.num_tensor, args.batch_lossz)):                        
+        for i in tqdm(range(0, self.num_first_dim, args.batch_lossz)):                        
             Vprod = self.V[0]
             for j in range(1, self.tensor.order-2):
                 Vprod = khatri_rao(Vprod, self.V[j])   # i_2 * ... * i_(m-1) x rank        
        
-            curr_batch_size = min(args.batch_lossz, self.tensor.num_tensor - i) 
+            curr_batch_size = min(args.batch_lossz, self.num_first_dim - i) 
             assert(curr_batch_size > 1)
-            s_idx = self.U_mapping[self.U_sidx[i]:self.U_sidx[i+curr_batch_size]]
+            s_idx = self.U_mapping[i:i+curr_batch_size]
             curr_S = self.S[s_idx, :].unsqueeze(1)  # bs' x 1 x rank
             VS = Vprod.unsqueeze(0) * curr_S   # bs' x i_2 * ... * i_(m-1) x rank        
             VS = torch.transpose(VS, 1, 2)   # bs' x rank x i_2 * ... * i_(m-1)        
             
-            curr_U = self.U[self.U_sidx[i]:self.U_sidx[i+curr_batch_size]]    # bs' x rank
+            curr_U = self.U[i:i+curr_batch_size]    # bs' x rank
             if mode != "parafac2":
-                curr_mapping = self.mapping[self.U_sidx[i]:self.U_sidx[i+curr_batch_size]]   # bs'
+                curr_mapping = self.mapping[i:i+curr_batch_size]   # bs'
                 curr_U_cluster = self.centroids[curr_mapping]  # bs' x rank
                 if mode=="train":                
                     sg_part = (curr_U - curr_U_cluster).detach()
@@ -199,7 +208,7 @@ class parafac2:
                     curr_U = curr_U_cluster
              # curr_U: batch size x i_max x rank
             approx = torch.bmm(curr_U.unsqueeze(1), VS).squeeze()   # bs' x i_2 * ... * i_(m-1)                    
-            curr_tensor = self.set_curr_tensor(curr_batch_size, i)  # bs' x i_2 * ... * i_(m-1)                                
+            curr_tensor = self.set_curr_tensor_new(curr_batch_size, i)  # bs' x i_2 * ... * i_(m-1)                                
             #curr_loss = torch.sum(torch.square(approx))
             curr_loss = torch.sum(torch.square(approx - curr_tensor))
             if is_train:
